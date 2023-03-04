acf03d61f5cc21b263c4856ac513cab6dfc34dca
Jaydon Krooss <jkrooss@gmail.com>
2023-03-04 18:14:59 -0500

    add firebase to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { initializeApp } from 'firebase/app';
+ 
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB4yCbe_4m3nGQDB864NtYEdU7xYpDh9bg",
+  authDomain: "dark-carport-268901.firebaseapp.com",
+  projectId: "dark-carport-268901",
+  storageBucket: "dark-carport-268901.appspot.com",
+  messagingSenderId: "575650731597",
+  appId: "1:575650731597:web:e003614d02bd555934ee04",
+  measurementId: "G-LN4L2WECK2"
+};
+
+initializeApp(firebaseConfig);
+
 
 function App() {
   const [count, setCount] = useState(0)
